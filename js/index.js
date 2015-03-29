fa7e10751bbc3f14c873c95f460a9ce43493e848
yuetsh <517252939@qq.com>
2015-03-29 15:52:17 +0800

    fix Firefox animation bug

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ $(function () {
     $(".slide-2-7").hover(function () {
         $("#image-3-p3").removeClass("hover-effect-5 iphone-content-5").addClass("hover-effect-4 iphone-content-5");
         $("#image-3").removeClass("hover-effect-3 iphone-content-6").addClass("hover-effect-2 iphone-content-6");
-        $("#right-p3").removeClass("hover-effect-5 slide-2-9").addClass("hover-effect-4 slide-2-9");
+        $("#right-p3").removeClass("hover-effect-5 slide-2-10").addClass("hover-effect-4 slide-2-10");
     }, function () {
         $("#image-3-p3").removeClass("hover-effect-4 iphone-content-5").addClass("hover-effect-5 iphone-content-5");
         $("#image-3").removeClass("hover-effect-2 iphone-content-6").addClass("hover-effect-3 iphone-content-6");
